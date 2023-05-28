5a589c52fa50fef430eeb71002c4becf10ae07d9
pablo-ramos-hernandez <126242940+pablo-ramos-hernandez@users.noreply.github.com>
2023-05-28 15:26:33 +0100

    feat(Personaje):Creacion de la clase, atributos y constructor

---
+++ src/Personaje.java
@@ -0,0 +1,12 @@
+public class Personaje {
+    private int salud;
+    private int defensa;
+    private int fuerza;
+
+    public Personaje(int defensa, int fuerza) {
+        this.defensa = defensa;
+        this.fuerza = fuerza;
+        this.salud = 12;
+    }
+
+}
